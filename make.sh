8c7b0ccfc2dc347f583deda50067bd51c9e4ebc5
Rellikeht <michaelhemperek@gmail.com>
2026-02-21 13:14:09 +0100

    improve build helpers

--- make.sh
+++ make.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 
-THR=4
-if [ "$1" -gt 1 ] 2>/dev/null; then
-    THR="$1"
-fi
-
 if [ "${PWD##*/}" != "svim" ]; then
     cd svim || exit
 fi
 
 cp ../feature.h src/
-# ../conf.sh /usr/local/bin --includedir=/usr/include && make -j"$THR"
-../conf.sh /usr/local/bin && make -j"$THR"
+# ../conf.sh /usr/local --includedir=/usr/include && make -j"$THR"
+../conf.sh /usr/local && make "$@"
